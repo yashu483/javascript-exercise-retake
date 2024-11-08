9de94d83728f1800f32d50e5b8f83b514f175edd
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-08 10:56:33 +0530

    Complete 04_removeFromarray exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function (array , ...args) {
+//     const newArray = [] ;
+
+//     array.forEach(element => {
+//         if ( !args.includes(element)) {
+//             newArray.push(element) ;
+//         }
+//     });
+
+//     return newArray ;
+// }
+const removeFromArray = function (array, first , second ,third , fourth) {
+
+    function checkItems(item) {
+        if (item === first || item === second || item === third || item === fourth) {
+            return false
+        }
+        else { return true; }
+    }
+    const newArray = array.filter(checkItems) ;
+    return newArray;
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
