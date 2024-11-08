69ed9cebf2a800279f6588805a796b2f92113d06
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-08 15:31:52 +0530

    complete 09_palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+let removedString = string.replace(/[\W_]+/g, "").toLowerCase() ;
+let stringReversed = string.split("").reverse().join("") ;
 
+if (removedString === stringReversed) {
+    return true ;
+} 
+else { return false ;}
 };
 
 // Do not edit below this line
