9b374eedc456f7ffe9b79eadea623ca40a1a00f1
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-08 17:07:24 +0530

    last 12_findTheOldest exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
 
+    function getOldestPeople(item) {
+        if (item.yearOfDeath === undefined) {
+            item.yearOfDeath = new Date().getFullYear();
+        };
+        if ((item.yearOfDeath - item.yearOfBirth) === Math.max(
+            array[0].yearOfDeath - array[0].yearOfBirth,
+            array[1].yearOfDeath - array[1].yearOfBirth,
+            array[2].yearOfDeath - array[2].yearOfBirth,
+
+        )
+        ) {
+            return true;
+        }
+        else { return false };
+
+    };
+    const isOldest = array.filter(getOldestPeople);
+    return isOldest[0];
 };
 
 // Do not edit below this line
