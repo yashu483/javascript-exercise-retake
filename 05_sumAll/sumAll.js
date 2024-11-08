46f2e96bdeb2d41a22664b184d034e5d16ba7e1a
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-08 12:04:01 +0530

    complete 05_sumAll exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+    if (typeof first !== "number" ||
+        typeof second !== "number" ||
+        !Number.isInteger(first) ||
+        !Number.isInteger(second) ||
+        first < 0 ||
+        second < 0
+    ) {
+        return "ERROR";
+    }
 
+    let bigNum = Math.max(first, second);
+    let smallNum = Math.min(first, second);
+
+    let sum = 0;
+
+    for (smallNum; smallNum <= bigNum; smallNum++) {
+        sum += smallNum ;
+    }
+    return sum ;
 };
 
 // Do not edit below this line
